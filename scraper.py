03d436a797fb5def7ce1262a40d740e0f7a09d92
yousefnegmeldin <yousefnegm@gmail.com>
2024-06-16 13:11:25 +0300

    fetched chemistry

--- scraper.py
+++ scraper.py
@@ -4,18 +4,20 @@ import os
 import time
 # url = "https://pastpapers.co/cie/IGCSE/Computer-Science-0478/2022-May-June/0478_s22_ms_12.pdf"
 
-subject = ["Computer-Science-0478"]
+# subject = ["Computer-Science-0478"]
+subject = ["Chemistry-0620"]
 
-year = ["2017","2018","2019", "2020", "2021", "2023"]
+year = ["2018","2019", "2020", "2021", "2022","2023"]
 
 
 
-session = ["May-June"]
+session = ["May-June", "Oct-Nov","March"]
 
 
 qp_or_ms = ["qp", "ms"]
 
-variant = ["11", "12", "13", "21", "22", "23"]
+# variant = ["11", "12", "13", "21", "22", "23"]
+variant = ["21","22","23","41","42","43","61","62","63"]
 
 
 subject_code = "0478"
@@ -35,14 +37,14 @@ for subj in subject:
                     winter_or_summer = winter_or_summer+y[2:]
 
                     time.sleep(1)
-                    url = f"https://pastpapers.co/cie/IGCSE/{subj}/{y}-{s}/0478_{winter_or_summer}_{q}_{v}.pdf"
+                    url = f"https://pastpapers.co/cie/IGCSE/{subj}/{y}-{s}/0620_{winter_or_summer}_{q}_{v}.pdf"
                     response = requests.get(url)
                     if response.status_code == 200:
                         # Open PDF file in wb mode
                         subfolder = f"{y}-{s}"
                         second_subfolder = "Question_Papers" if q == "qp" else "Mark_Schemes"
-                        os.makedirs(os.path.join("0478_Computer_Science", subfolder, second_subfolder), exist_ok=True)
-                        with open(os.path.join("0478_Computer_Science",subfolder, second_subfolder, f"{subject_code}_{y}_{s}_{q}_{v}.pdf"), 'wb') as f:
+                        os.makedirs(os.path.join("0620_Chemistry", subfolder, second_subfolder), exist_ok=True)
+                        with open(os.path.join("0620_Chemistry",subfolder, second_subfolder, f"{subject_code}_{y}_{s}_{q}_{v}.pdf"), 'wb') as f:
                             # Write the content of the response to the file
                             f.write(response.content)
                     else:
